3050a817db4bec227757443cfeee9d9667a9745c
KetaEs <eswar-chand.keta@basf.com>
2021-06-13 12:57:01 +0200

    added header component

---
+++ src/common/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = function(prop) {
+       return (
+           <div className="app-header">
+               <div className="app-title">Image Viewer</div>
+           </div>
+       )
+}
+
+export default Header;
