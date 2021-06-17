47e4f63c4e5af932010ad2083a9a59c09aa8df65
KetaEs <eswar-chand.keta@basf.com>
2021-06-17 04:08:07 +0200

    addedd searchbar and profilepic in header

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
+import SearchIcon from '@material-ui/icons/Search';
+import { Input, InputAdornment } from "@material-ui/core";
 import './Header.css';
+import ProfilePic from "../profilePic/ProfilePic";
 
-const Header = function(prop) {
-       return (
-           <div className="app-header">
-               <div className="app-title">Image Viewer</div>
-           </div>
-       )
+class Header extends Component {
+    constructor() {
+        super();
+    }
+
+    onSearch = async (e) => {
+        console.log(e.target.value);
+        console.log(this.props);
+    }
+
+    render() {
+        return (
+            <div className="app-header">
+                <div className="app-title">Image Viewer</div>
+                <div className="app-searchbar-icon">
+                    {
+                        this.props.showSearchbar ? 
+                        <div>
+                            <Input
+                                className="search-box"
+                                type="search"
+                                placeholder="Search..."
+                                disableUnderline
+                                startAdornment={
+                                <InputAdornment position="start">
+                                    <SearchIcon />
+                                </InputAdornment>
+                                }
+                                onChange={this.onSearch}
+                            /> 
+                            <ProfilePic {...this.props} />
+                        </div>
+                        : null
+                    } 
+
+                </div>
+
+            </div>
+        )
+    }
 }
 
 export default Header;
